956df6c472650e6a0d23b6131731aba367e56795
Abraham O. OLAGUNJU <36889745+OluwaninsolaAO@users.noreply.github.com>
2023-02-08 17:10:01 +0100

    Fix: kwargs condition block in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -10,17 +10,18 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """initialize the instance attributes"""
-        if kwargs is not None:
+        
+        if len(kwargs) != 0:
             kwargs.pop('__class__', None)
             for key, value in kwargs.items():
                 if key in ["created_at", "updated_at"]:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
-                return
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     # basic class methods
     def __str__(self):
